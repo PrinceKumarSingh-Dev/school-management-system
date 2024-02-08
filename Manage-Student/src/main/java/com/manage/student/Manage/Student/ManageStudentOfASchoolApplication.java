102b9a8755d694c5b1afb9ca8379db84e9208ffd
PrinceKumarSingh-Dev <957prince@gmail.com>
2024-02-08 16:55:40 +0530

    Initial commit

---
+++ Manage-Student/src/main/java/com/manage/student/Manage/Student/ManageStudentOfASchoolApplication.java
@@ -0,0 +1,13 @@
+package com.manage.student.Manage.Student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageStudentOfASchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageStudentOfASchoolApplication.class, args);
+	}
+
+}
---
+++ Manage-Student/src/test/java/com/manage/student/Manage/Student/ManageStudentOfASchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manage.student.Manage.Student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageStudentOfASchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
